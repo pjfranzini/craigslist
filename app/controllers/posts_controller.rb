543a9925314950d3c25dfe4952c57f7e248e445a
Paula_Franzini <pjfranzini@hotmail.com>
2013-07-29 14:19:55 -0400

    nearly done with lvl 1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,20 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: 'Post was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def edit
@@ -20,4 +28,10 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body, :location, :email, :price)
+    end
 end
+
